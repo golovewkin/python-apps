18b1f313bb736ab5640a71ed827b420199c59660
golovewkin <golovewkin@gmail.com>
2023-08-18 20:04:19 -0600

    base files add

---
+++ weather/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import requests
+from .forms import CityForm
+
+def get_weather(request):
+    form = CityForm(request.GET or None)
+
+    if form.is_valid():
+        city = form.cleaned_data['city']
+        api_key = settings.WEATHER_API_KEY
+        api_url = settings.WEATHER_API_URL
+
+        response = requests.get(api_url, params={'key': api_key, 'q': city})
+        weather_data = response.json()
+
+        context = {
+            'city': city,
+            'temperature': weather_data['current']['temp_c'],
+            'condition': weather_data['current']['condition']['text'],
+        }
+    else:
+        context = {'form': form}
+
+    return render(request, '../templates/weather/weather.html', context)
