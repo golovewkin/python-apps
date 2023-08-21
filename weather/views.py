03edbabf9777ccae04e70d8120b9127d0764c21d
golovewkin <golovewkin@gmail.com>
2023-08-20 20:04:34 -0600

    try catch add

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from django.urls import reverse
 import requests
 
 
@@ -8,27 +7,28 @@ def get_weather_by_city(request):
     # return HttpResponse(city)
     city = request.GET.get("city")
     if city is not None:
-        # city = request.GET.get('city', city)  # Default city if not provided
-        # TODO get key from settings
-        api_key = 'settings.WEATHER_API_KEY'
-        api_url = 'settings.WEATHER_API_UR'
+        try:
+            # TODO get key from settings
+            api_key = 'settings.WEATHER_API_KEY'
+            api_url = 'settings.WEATHER_API_URI'
 
-        # response = requests.get(api_url, params={'key': api_key, 'q': city})
-        # weather_data = response.json()
+            response = requests.get(api_url, params={'key': api_key, 'q': city})
+            weather_data = response.json()
+            if response.status_code == 200:
+                data = response.json()
+                temp = data['main']['temp']
+                desc = data['weather'][0]['description']
+                print(f'Temperature: {temp} K')
+                print(f'Description: {desc}')
+            else:
+                raise Exception("Sorry, something went wrong")
+        except:
+            print("An exception occurred")
 
-    # context = {
-    #     'city': city,
-    #     'temperature': weather_data['current']['temp_c'],
-    #     'condition': weather_data['current']['condition']['text'],
-    # }
+    context = {
+        'city': city,
+        # 'temperature': weather_data['current']['temp_c'],
+        # 'condition': weather_data['current']['condition']['text'],
+    }
 
-    # if response.status_code == 200:
-    #     data = response.json()
-    #     temp = data['main']['temp']
-    #     desc = data['weather'][0]['description']
-    #     print(f'Temperature: {temp} K')
-    #     print(f'Description: {desc}')
-    # else:
-    #     return HttpResponse('Error fetching weather data')
-
-    return render(request, 'weather/weather.html', {'city': city})
+    return render(request, 'weather/weather.html', {'city': context})
